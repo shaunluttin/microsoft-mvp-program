99ba57b82cecd8078d448bb1a191d8e64140da98
Shaun Luttin <shaunluttin@gmail.com>
2018-03-07 13:06:05 -0800

    Make get-contributions.ps1 prettier

--- mvp-award-contributions/get-contributions.ps1
+++ mvp-award-contributions/get-contributions.ps1
@@ -1,34 +1,52 @@
-
 #
-# One alternative is to use the same bearer token that the Microsoft MVP API uses in its Try It user interface.
-# 1. Go to https://mvpapi.portal.azure-api.net/Products/ > MVP Production > MVP Production > GetContributions > Try It
-# Then set Authorization to Authorization Code.
-# Then copy the resultant Authorizatio header from the UI.
+# Authenticate
+# One way to acquire the bearer token is to use the Microsoft MVP API Try It GUI.
+# 1. Go to https://mvpapi.portal.azure-api.net/Products > MVP Production > MVP Production > GetContributions > Try It.
+# 2. Then set Authorization drop down list to Authorization Code.
+# 3. Then copy the resultant Authorizatio header from the UI.
+# 
+# Another way would be 
 #
 
 $subscriptionKey = $env:MVP_SUBSCRIPTION_KEY;
 $bearerToken = $env:MVP_BEARER_TOKEN;
-
-#
-# Assume the award cycle is from April 01 to March 31.
-#
-$currentCycleStartDate = "2017-04-01"; 
 
 if (($subscriptionKey -eq $null) -or ($bearerToken -eq $null)) {
     Write-Output "Please set the MVP_SUBSCRIPTION_KEY and MVP_BEARER_TOKEN environmental variables.";
     exit;
 }
 
+#
+# Set some reasonable constant values.
+# E.g. Assume the award cycle is from April 01 to March 31.
+#
+
+$cycleStartDate = "2017-04-01"; 
 $offset = 0;
-$limit = 100;
+$limit = 500;
+
+#
+# Fetch the contributions.
+#
 
 $headers = @{};
 $headers["Ocp-Apim-Subscription-Key"] = $subscriptionKey;
 $headers["Authorization"] = "Bearer $bearerToken";
 
 $response = Invoke-WebRequest -UseBasicParsing "https://mvpapi.azure-api.net/mvp/api/contributions/$offset/$limit" -Headers $headers
-
 $contributions = $response | Select-Object -ExpandProperty Content | ConvertFrom-Json | Select-Object -ExpandProperty Contributions
 
-$currentCycleContributions = $contributions | Where-Object { (Get-Date $_.StartDate ) -gt (Get-Date "2017-03-31") }
+#
+# Filter the contributions.
+#
+
+$contributions | Where-Object { (Get-Date $_.StartDate ) -gt (Get-Date $cycleStartDate) }
+
+#
+# Make the contributions kind of prettier.
+#
+
+$contributions | Select-Object ContributionTypeName, Title, Description, ReferenceUrl, AnnualQuantity, StartDate | Format-List
+
+
 
